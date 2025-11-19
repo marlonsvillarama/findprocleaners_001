83e0b171ea48fb13bc7b8c70ab38925fdd7ffd64
Marlon Villarama <marlonsvillarama@gmail.com>
2025-11-20 01:06:56 +1300

    20251120

--- src/lib/components/root/product-preview.svelte
+++ src/lib/components/root/product-preview.svelte
@@ -11,7 +11,7 @@
     <!-- </div> -->
     <a href={data.link} target="_blank" class="px-3 py-2 bg-green-700 rounded-lg text-sm text-white font-medium hover:bg-green-800">Buy this item</a>
     <div class="grid text-left">
-        <p class="text-xs leading-[1.25rem] text-gray-500 font-light max-h-[100px] overflow-hidden text-ellipses">{data.description}</p>
+        <p class="text-xs leading-[1.25rem] text-gray-800 font-extralight max-h-[100px] overflow-hidden text-ellipses">{data.description}</p>
         <span>...</span>
     </div>
 </div>
